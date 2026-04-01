d2b5f486d6f1ac40ca0855b0c6da6cffe4fe87d7
fauzanodji-beep <fauzanodji@gmail.com>
2026-04-02 00:46:17 +0700

    Overloading

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+// =======================
+// OVERLOADING
+// =======================
+class ImageProcessor
+{
+    // Resize berdasarkan persen
+    public void Resize(int persen)
+    {
+        if (persen < 100)
+            Console.WriteLine("Gambar diperkecil menjadi " + persen + "%");
+        else
+            Console.WriteLine("Gambar diperbesar menjadi " + persen + "%");
+    }
+
+    // Resize berdasarkan lebar & tinggi
+    public void Resize(int lebar, int tinggi)
+    {
+        Console.WriteLine($"Gambar diubah ke ukuran {lebar} x {tinggi}");
+    }
+
+    // Resize berdasarkan ukuran kertas
+    public void Resize(string ukuranKertas)
+    {
+        Console.WriteLine("Gambar disesuaikan dengan ukuran kertas: " + ukuranKertas);
+    }
+}
+
